380b35097452374d32e5dc3a55108aa435083cd2
DilmiDevindi <dilmidliyanag@gmail.com>
2026-05-03 21:15:44 +0530

    Create event_repository

--- lib/domain/repositories/event_repository.dart
+++ lib/domain/repositories/event_repository.dart
@@ -2,5 +2,13 @@
 // File: event_repository.dart
 // Assigned to: Member 3 (Database & Data Layer)
 // ============================================================
+import '../entities/event.dart';
 
-
+// The Interface (Contract)
+abstract class EventRepository {
+  Stream<List<Event>> getAllEvents();
+  Future<void> saveEvent(Event event);
+  Future<void> deleteEvent(String id);
+  Future<void> toggleFavorite(Event event, String userId);
+  Future<List<Event>> getBookmarkedEvents(String userId);
+}
