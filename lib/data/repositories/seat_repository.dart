52dd374e6aa62a9d64973d0a29db156a68c713b9
DilmiDevindi <dilmidliyanag@gmail.com>
2026-05-03 21:30:49 +0530

    create seat_repository

--- lib/data/repositories/seat_repository.dart
+++ lib/data/repositories/seat_repository.dart
@@ -2,5 +2,24 @@
 // File: seat_repository.dart
 // Assigned to: Member 3 (Database & Data Layer)
 // ============================================================
+import '../datasources/local_database.dart';
 
+class SeatRepository {
+  final LocalDatabase _dbHelper = LocalDatabase.instance;
+
+  //Initialize seats for an event (Relation logic)
+  Future<void> initializeSeats(String eventId, int count) async {
+    final db = await _dbHelper.database;
+    final batch = db.batch();
+    
+    for (int i = 1; i <= count; i++) {
+      batch.insert('local_tickets', {
+        'ticketId': '${eventId}_$i',
+        'eventId': eventId,
+        'seatNumber': i,
+      });
+    }
+    await batch.commit();
+  }
+}
 
