1393a606ea8077b12ef4250715d5dedd9e6bf226
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2026-05-04 20:01:05 +0530

    Reorganize comments and provider declaration in organizer_provider.dart

--- lib/presentation/providers/organizer_provider.dart
+++ lib/presentation/providers/organizer_provider.dart
@@ -1,3 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// 1. LOGIC PROVIDER
+final organizerLogicProvider = Provider((ref) => OrganizerLogic());
+
