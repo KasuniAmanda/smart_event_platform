8788354645028ba4606ca95a971c01e429380906
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2026-05-04 19:52:05 +0530

    Add Firestore instance initialization in event_provider.dart

--- lib/presentation/providers/event_provider.dart
+++ lib/presentation/providers/event_provider.dart
@@ -1,3 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../domain/entities/event.dart';
+
+// FIRESTORE INSTANCE
+
+final FirebaseFirestore _db = FirebaseFirestore.instance;
+
